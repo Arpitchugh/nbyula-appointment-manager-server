d0cc89d4db509df4dd93e400b59172da5ffe9027
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2022-09-19 14:17:17 +0530

    fix: :bug: user is required to login before updating name

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,9 +3,13 @@ import {
 	getAllUsersHandler,
 	patchUserName,
 } from '../controller/user.controller.js';
+import requireLogin from '../middleware/requireLogin.middleware.js';
 
 const userRouter = Router();
 
-userRouter.route('/users').get(getAllUsersHandler).patch(patchUserName);
+userRouter
+	.route('/users')
+	.get(getAllUsersHandler)
+	.patch(requireLogin, patchUserName);
 
 export default userRouter;
